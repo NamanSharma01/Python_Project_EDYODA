152053a26f8afad7de975092a12de6bf83609608
NamanSharma01 <95707898+NamanSharma01@users.noreply.github.com>
2022-02-01 00:50:28 +0530

    Add files via upload

---
+++ Project.code.py
@@ -0,0 +1,165 @@
+#Resgistration process:
+#USER INFO
+import zomato
+
+class customer:
+    def __init__(self,c1,name,phone_number,email,address,password):
+        self.c1 = customer_1
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
+        self.password = password      
+    def phone_number(self):
+        self.phone_number = input('Enter your phone number: ')
+    def email(self):
+        self.email = input('Enter your Email: ')
+    def address(self):
+        self.address = input('Enter your address')
+    def password_user(self):
+        self.password = input('Enter your password')
+admin=['Naman','Rohan','Tykon','Ashneel','Namita']
+password=[]
+for i in admin:
+    if i in admin:
+        
+        print('you can login')
+    else:
+        print("Register yourself")
+
+def log_in(Log_In_Admin,Password_Admin):
+    pw=login_info.get(Log_In_Admin)
+    if pw!=Password_Admin:
+        print('wrong username or password')
+        return False
+    else:
+        return True
+li=[]
+yes_condition='y'
+while yes_condition=='y':
+    print("LIST OPERATIONS")
+    print("1. Add FoodItems")
+    print("2. Edit the list")
+    print("3. Remove an item from the list")
+    print("4. Insertion of new fooditem into the list")
+    print("5. View the final list of fooodItem")
+
+    ch=int(input("Enter your choice:"))
+    if ch==1:
+        size=int(input("Enter the numbers of items you want to order:"))
+        for x in range(size):
+            val=(input("Enter the name of foodItem:"))
+            li.append(val)
+        print(li)
+    if ch==2:
+        print(li)
+        pos=int(input("Enter the position to be updated:"))
+        val=(input("Enter new food item:"))
+        li[pos]=val
+        print("Record updated:")
+        print(li)
+    if ch==3:
+        print(li)
+        pos=int(input("Enter the position to be deleted:"))
+        li.pop(pos) 
+        print("After deletion list is:",li)
+    if ch==4:
+        print(li)
+        pos=int(input("Enter the position:"))
+        val=(input("Enter the Food name:"))
+        li.insert(pos,val)
+        print("After insertion list is ",li)
+    if ch==5:
+        print("Final list of foodItems are:")
+        for x in li:
+            print(x)
+    yes_condition=input("Do you want to continue?[y/n]:")
+
+foods=['Tandoori Chicken','Vegan Burger','Truffle Cake']
+New_List={'A102':'Vegan Burger',
+            'A101':'Tandoori Chicken',
+            'A103':'Truffle Cake'}
+command=input("Enter the food ID you want to remove from the dict: ")
+indexpop = foods.index(New_List.get(command))
+
+foods.pop(indexpop)
+print(foods)
+    
+Full_Name=input("Enter your full Name: ")
+Phone_No=int(input("Enter your Conatct Number: "))
+Email=input("Enter your email Id: ")
+Address=input("Enter your address: ")
+Password=input("Save your unique password, for future use")
+login_info={'Naman':'#123',
+            'krunal':'3333',       #UserInfo
+            'Kartik':'3322'}
+
+
+
+Log_In_User=input("Enter your UserId: ")
+Password_User=input("Enter your password")
+            
+
+Second_li=[]
+yes_con='y'
+while yes_con=='y':
+    print("1.Place New Order.\n2.Update Profile\n3.Order History")
+
+
+    choice=int(input("Enter your choice:"))
+    if choice==1:
+        size_new=int(input("Enter the numbers of items you want to order:"))
+        for i in range(size_new):
+            value=(input("Enter the name of foodItem:"))
+            Second_li.append(value)
+        print(Second_li)
+        print('Order Placed')
+    if choice==2:
+        print(Second_li)
+        position=int(input("Enter the position  to be updated:"))
+        value=(input("Enter new item:"))
+        Second_li[position]=value
+        print("Record updated:")
+        print(Second_li)
+    yes_con=input("Do you want to continue?[y/n]:")
+
+List_of_food_items={'Name':'Tandoori Chicken',
+                     'Quantity':'4 pieces',
+                     'Price': 'INR 240',
+                     'Id':'A101',
+                     'discount_rate_1':0.05},
+{'Name':'Vegan Burger',
+                        'Quantity':'1 piece',
+                     'Price': 'INR 320',
+                     'Id':'A102',
+                      'discount_rate_2':0.10},
+{'Name':'Truffle Cake',
+                    'Quantity':'500 gms',
+                     'Price': 'INR 900',
+                     'discount_rate_3':0.15,
+                     'Id':'A103'}
+
+
+foods=['Tandoori Chicken','Vegan Burger','Truffle Cake']
+x=int(input("How many items: "))
+y=[]
+placed_order=[]
+for i in range(0,x):
+    z=int(input("Enter your item number which you want to order: "))
+    y.append(z)
+    temp=foods[y[i]]
+    placed_order.append(temp)
+print(placed_order)
+
+
+
+
+
+
+
+
+
+
+
+
+
